ad409104f220bcbfd2d1ced18991c948fc006c42
sushant102004 <sushant.dhiman9812@gmail.com>
2023-05-29 17:46:55 +0530

    feat(Mongoose.ts) - added class to connect to MongoDB

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { Logger } from './src/utils/loaders/Logger'
+import { DB } from './src/utils/loaders/Mongoose'
 
 const server = async (): Promise<express.Application> => {
     const app = express()
@@ -9,6 +10,8 @@ const server = async (): Promise<express.Application> => {
 
     /* Things that are required to initialized before the server starts can come here.
     Loggers & Middlewares */
+    DB.connectToDB()
+
 
     return app
 }
---
+++ src/utils/loaders/Mongoose.ts
@@ -0,0 +1,20 @@
+import mongoose, { ConnectOptions } from 'mongoose'
+import { Logger } from './Logger'
+
+export class DB {
+    static async connectToDB() {
+        try {
+            const mongoDB_URI = process.env.MONGODB_URI
+
+            if (mongoDB_URI) {
+                await mongoose.connect(mongoDB_URI)
+                Logger.instance.info(`Connected to MongoDB`)
+            } else {
+                Logger.instance.error('MongoDB URI not specified')
+            }
+        } catch (err) {
+            Logger.instance.error(`Error: ${err}`)
+            process.exit(1)
+        }
+    }
+}
