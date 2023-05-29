12bbf28c527b271e7de3ac1dee90a20d17fa0937
sushant102004 <sushant.dhiman9812@gmail.com>
2023-05-29 15:10:44 +0530

    feat(Logger.ts) - Added winston logger service

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import server from './server'
+import { Logger } from './src/utils/loaders/Logger'
 
 (async () => {
     const app = await server()
     const PORT = process.env.PORT || 3000
 
     app.listen(PORT, () => {
-        console.log(`listening on port ${PORT}`)
+        Logger.instance.info(`Server listening on ${PORT}`)
     })
 
 })()
--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { Logger } from './src/utils/loaders/Logger'
 
 const server = async (): Promise<express.Application> => {
     const app = express()
     dotenv.config()
-    
+    Logger.Loader()
+
     /* Things that are required to initialized before the server starts can come here.
     Loggers & Middlewares */
 
