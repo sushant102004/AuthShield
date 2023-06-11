c41c15c6dd5648e68758162fb9b20874d5934c43
sushant102004 <sushant.dhiman9812@gmail.com>
2023-06-11 10:37:58 +0530

    Error  Handling for Sign Up

--- src/utils/tools/errorController.ts
+++ src/utils/tools/errorController.ts
@@ -8,12 +8,23 @@ export class ErrorControllerClass extends ErrorClass {
 
         const sCode = parseInt(err.statusCode)
 
+
+
+        if (err.message.split(' ')[0] == 'E11000') {
+            res.status(409).json({
+                status: 'fail',
+                message: 'Duplicate Value',
+                statusCode: 409,
+            })
+
+            return
+        }
+
+
         res.status(sCode).json({
             status: err.status,
             message: err.message,
-            // err: err,
             statusCode: err.statusCode,
-            stack: err.stack
         })
 
     }
